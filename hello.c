e5a9b52651a03c16d448820bd6923936f1a1329b
DorskFR <42279120+DorskFR@users.noreply.github.com>
2020-08-31 12:58:20 +0900

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
+
+// Forgot this assignment :-)
+
+int main(void)
+{
+    string name = get_string("What is your name:?\n");
+    printf("hello, %s\n", name);
+}
