7cd995d0cd2178824de64887b2efcd523a0df1cb
DorskFR <42279120+DorskFR@users.noreply.github.com>
2020-09-06 18:44:35 +0900

    Add files via upload

---
+++ pset7/movies/13.sql
@@ -0,0 +1,1 @@
+SELECT DISTINCT name FROM people INNER JOIN stars ON stars.person_id=people.id WHERE stars.movie_id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Kevin Bacon")) AND person_id != (SELECT id FROM people WHERE name = "Kevin Bacon") ORDER BY name;
